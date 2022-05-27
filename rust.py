f2ae325d2c035a775726c662edb5a9d25d50bb79
julian <j.feinauer@pragmaticminds.de>
2022-05-27 12:32:58 +0200

    added typedef to make it more elegant

--- rust.py
+++ rust.py
@@ -3,10 +3,12 @@ from cffi import FFI
 ffibuilder = FFI()
 
 ffibuilder.cdef("""
+    typedef void* point;
+    
     void say_hello();
-    void* point_new(int x, int y);
-    void point_print(void* point);
-    void point_free(void* point);
+    point point_new(int x, int y);
+    void point_print(point p);
+    void point_free(point p);
 """)
 
 lib = ffibuilder.dlopen("rust/target/release/librust.dylib")
